41a6de79bf5e63fb0980b576b4d76c49eb34fa05
nmd <adam.farago.nmd@gmail.com>
2020-07-28 12:35:59 +0200

    ADD basic clang format

--- src/test/java/jin/HelloLWJGLTest.java
+++ src/test/java/jin/HelloLWJGLTest.java
@@ -5,10 +5,9 @@ import static org.junit.Assert.*;
 
 public
 class HelloLWJGLTest {
-  @Test public void testHelloLWJGLHasWindow() {
-    HelloLWJGL classUnderTest = new HelloLWJGL();
-    classUnderTest.init();
-    assertTrue("HelloLWJGL must have window after initialization",
-               classUnderTest.getWindow() != 0);
-  }
+	@Test public void testHelloLWJGLHasWindow() {
+		HelloLWJGL classUnderTest = new HelloLWJGL();
+		classUnderTest.init();
+		assertTrue("HelloLWJGL must have window after initialization", classUnderTest.getWindow() != 0);
+	}
 }
