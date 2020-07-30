b3b400d3c20e346bea39b5e093442a3a343de718
nmd <adam.farago.nmd@gmail.com>
2020-07-30 12:07:32 +0200

    ADD basic window skeleton

---
+++ src/main/java/jin/view/Window.java
@@ -0,0 +1,87 @@
+package jin.view;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.glViewport;
+import static org.lwjgl.system.MemoryUtil.*;
+
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+/**
+ * <p>Class representing a GLWF window and add a basic interface to interact with.
+ * <i>Based ont Thinkmatrix tutorials: <a
+ * href="https://youtu.be/VS8wlS9hF8E?list=PLRIWtICgwaX0u7Rf9zkZhLoLuZVfUksDP">link to youtube</a></i>
+ * @author nmd
+ * @version 1.0
+ * @since 2020-07-30
+ */
+public class Window {
+	private long windowId;
+	public long getWindowId() { return windowId; }
+
+	/**
+	 * <p>Initialize GLFW and create a window
+	 * @param title Initial window title
+	 * @param width Initial window width
+	 * @param height Initial window Height
+	 */
+	public Window(String title, int width, int height) throws IllegalStateException {
+		initializeGlfw();
+		setWindowHints();
+		createWindow(title, width, height);
+		finalizeWindow();
+		setCallbacks();
+	}
+
+	/**
+	 * <p>Swap window buffers
+	 */
+	public void swap() { glfwSwapBuffers(windowId); }
+
+	/**
+	 * <p>Check window should close
+	 * @return true if still running, false if should close
+	 */
+	public boolean isRunning() { return !glfwWindowShouldClose(windowId); }
+
+	/**
+	 * <p>Handle events, here is where our callbacks would be called.
+	 */
+	public void pollEvents() { glfwPollEvents(); }
+
+	private void finalizeWindow() {
+		glfwMakeContextCurrent(windowId);
+		glfwShowWindow(windowId);
+		GL.createCapabilities(); // XXX this will be a headache here later (threading issues)
+	}
+
+	private void setCallbacks() {
+		glfwSetKeyCallback(windowId, this::keyChangedCallback);
+		glfwSetFramebufferSizeCallback(windowId, this::frameBufferChangedCallback);
+	}
+
+	private void createWindow(String title, int width, int height) {
+		windowId = glfwCreateWindow(width, height, title, NULL, NULL);
+		if (NULL == windowId)
+			throw new IllegalStateException("glfwCreateWindow() failed!");
+	}
+
+	private void setWindowHints() {
+		glfwDefaultWindowHints();
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+	}
+
+	private void initializeGlfw() {
+		GLFWErrorCallback.createPrint(System.err).set();
+		if (!glfwInit())
+			throw new IllegalStateException("glfwInit() failed!");
+	}
+
+	private void keyChangedCallback(long windowId, int key, int scancode, int action, int mods) {
+		if ((key == GLFW_KEY_ESCAPE || key == GLFW_KEY_Q) && action == GLFW_RELEASE)
+			glfwSetWindowShouldClose(windowId, true);
+	}
+
+	private void frameBufferChangedCallback(long windowId, int width, int height) { glViewport(0, 0, width, height); }
+}
