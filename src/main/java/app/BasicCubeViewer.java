a6af4ad39fd20ee649343b4fd11ff57417991832
nmd <adam.farago.nmd@gmail.com>
2020-07-31 12:56:45 +0200

    INIT BasicCubeViewer skeleton

---
+++ src/main/java/app/BasicCubeViewer.java
@@ -0,0 +1,14 @@
+package app;
+
+import jin.controller.GameLoop;
+
+public class BasicCubeViewer extends GameLoop {
+
+	@Override
+	protected void create() {}
+
+	@Override
+	protected void update() {}
+
+	public static void main(String[] args) { new BasicCubeViewer().start(); }
+}
