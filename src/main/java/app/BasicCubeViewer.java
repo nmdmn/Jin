9dae8a3d05289502fb6c9a16c0b0a7d2227a56d0
nmd <adam.farago.nmd@gmail.com>
2020-07-31 17:01:52 +0200

    ADD vertices representing a cube

--- src/main/java/app/BasicCubeViewer.java
+++ src/main/java/app/BasicCubeViewer.java
@@ -1,14 +1,48 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
 import jin.controller.GameLoop;
+import org.joml.Vector3f;
 
+/**
+ * <p>Draw a cube
+ * Note that the vertex data is hardcoded
+ * @author nmd
+ * @version 1.0
+ * @since 2020-07-31
+ */
 public class BasicCubeViewer extends GameLoop {
+
+	private static Vertex[] cubeVertices = generateCubeVertices(1.f);
 
 	@Override
 	protected void setup() {}
 
 	@Override
 	protected void update() {}
+
+	static class Vertex {
+		public Vector3f position;
+		public Vector3f color;
+
+		public Vertex(Vector3f position, Vector3f color) {
+			this.position = position;
+			this.color = color;
+		}
+	}
+
+	private static Vertex[] generateCubeVertices(float size) {
+		Vertex[] vertices = {new Vertex(new Vector3f(-size, -size, size), new Vector3f(-size, -size, size)),
+							 new Vertex(new Vector3f(size, -size, size), new Vector3f(size, -size, size)),
+							 new Vertex(new Vector3f(-size, size, size), new Vector3f(-size, size, size)),
+							 new Vertex(new Vector3f(size, size, size), new Vector3f(size, size, size)),
+							 new Vertex(new Vector3f(-size, -size, -size), new Vector3f(-size, -size, -size)),
+							 new Vertex(new Vector3f(size, -size, -size), new Vector3f(size, -size, -size)),
+							 new Vertex(new Vector3f(-size, size, -size), new Vector3f(-size, size, -size)),
+							 new Vertex(new Vector3f(size, size, -size), new Vector3f(size, size, -size))};
+		return vertices;
+	}
 
 	public static void main(String[] args) { new BasicCubeViewer().start(); }
 }
